ce5da2fc95b56510545327f1d591558ab9032420
Matt George <mattmg412@gmail.com>
2024-04-03 21:45:59 -0700

    Cleanup

--- src/QuickOutline.ts
+++ src/QuickOutline.ts
@@ -1,149 +1,98 @@
-import { window, type SymbolInformation, SymbolKind, Selection, Range, Position, type QuickPickItem } from "vscode";
+import { window, type SymbolInformation, SymbolKind, Selection, Range, Position, type QuickPickItem, QuickInputButton, QuickPickItemKind, ThemeIcon, Uri, Location, TextDocument } from "vscode";
 import { selectionStyle } from "./extension";
+import { createSymbolFallbackDescription, iconForKind, pad } from "./utils";
 
 export type QuickItem = QuickPickItem & { symbol: SymbolInformation; };
 
+const expandedByDefaultTypes = [
+  SymbolKind.Class,
+  SymbolKind.Struct,
+  SymbolKind.Interface,
+  SymbolKind.Object
+];
+
+const ignoredTypesIfEmpty = [
+  SymbolKind.Module,
+  SymbolKind.Object
+];
+
+class QuickOutlineItem implements QuickPickItem {
+  constructor(
+    private _symbol: SymbolInformation,
+    private _depth = 0
+  ) {
+    const detail = "detail" in this._symbol &&
+      typeof this._symbol["detail"] === "string" &&
+      this._symbol.detail.length > 0 ? this._symbol.detail : null;
+
+    this.expanded = expandedByDefaultTypes.includes(this._symbol.kind);
+    this.description = detail ?? createSymbolFallbackDescription(this._symbol, window.activeTextEditor!);
+
+    const line = this._symbol.location.range.start.line;
+    const lineNumberFormatted = pad(line.toString());
+    const depthPadding = "".padEnd(this._depth * 4, " ");;
+
+    this.label = `${lineNumberFormatted} ${depthPadding} ${iconForKind(this._symbol.kind)} ${this._symbol.name}`;
+
+    if ("children" in _symbol) {
+      const children = (_symbol.children as any as SymbolInformation[]) || [];
+      // Symbols may not be returned to us sorted
+      children.sort((a, b) => a.location.range.start.line - b.location.range.start.line);
+
+      this.children = children.map(child => new QuickOutlineItem(child, this._depth + 1));
+    }
+  }
+
+  readonly children: QuickOutlineItem[] = [];
+  readonly description: string;
+  readonly label: string;
+  expanded: boolean;
+
+  get location(): Location {
+    return this._symbol.location;
+  }
+
+  get lineStart(): number {
+    return this.location.range.start.line;
+  }
+
+  get name(): string {
+    return this._symbol.name;
+  }
+
+  getNameRange(document: TextDocument): Range {
+    const startOffset = document.offsetAt(this.location.range.start);
+    const tokenOffset = document.getText(this.location.range)
+      .indexOf(this.name) + startOffset;
+    const tokenPosition = document.positionAt(tokenOffset);
+    const tokenRange = document.getWordRangeAtPosition(tokenPosition);
+    const end = new Position(tokenPosition.line, tokenPosition.character + 10);
+
+    return new Range(tokenPosition, end);
+  }
+}
+
+
 export class QuickOutline {
 
-  private _getItem(symbol: SymbolInformation, depth = 0): QuickItem {
-    const line = symbol.location.range.start.line;
-    let lineNumberFormatted = `${line}`;
-
-    // Simply calling pad does not seem to position correclty in the pick list...
-    if (lineNumberFormatted.length === 1) {
-      lineNumberFormatted += "        ";
-    }
-
-    if (lineNumberFormatted.length === 2) {
-      lineNumberFormatted += "      ";
-    }
-
-    if (lineNumberFormatted.length === 3) {
-      lineNumberFormatted += "    ";
-    }
-
-    if (lineNumberFormatted.length === 4) {
-      lineNumberFormatted += "   ";
-    }
-
-    const depthPadding = "".padEnd(depth * 4, " ");;
-    return {
-      label: `${lineNumberFormatted} ${depthPadding} ${this._iconForKind(symbol.kind)} ${symbol.name}`,
-      description: this._textForSymbol(symbol),
-      symbol
-    };
-  };
-
-  private _quickPick = window.createQuickPick<QuickItem>();
-
   constructor(
-    private readonly _symbols: SymbolInformation[]) {
+    symbols: SymbolInformation[]) {
 
     this._quickPick.placeholder = "Jump to a symbol";
     this._quickPick.matchOnDescription = true;
 
-    const items = [];
+    const items = symbols.map(symbol => new QuickOutlineItem(symbol));
+    // Symbols not guarenteed to be sorted
+    items.sort((a, b) => a.lineStart - b.lineStart);
+    this._quickPick.items = this._extractExpandedItems(items);
 
-    console.log((this._symbols));
-
-    for (const symbol of this._symbols) {
-      const item = this._getItem(symbol);
-
-      if (symbol.kind === SymbolKind.Module || symbol.kind === SymbolKind.Object) {
-        const parent = symbol as any;
-        if ("children" in parent && parent.children.length) {
-          // Only add the item if it is "interesting", e.g., it has children
-          items.push(item);
-
-          for (const child of (symbol as any).children) {
-            items.push(this._getItem(child, 1));
-          }
-        }
-      }
-
-      else if (symbol.kind === SymbolKind.Class || symbol.kind === SymbolKind.Struct || symbol.kind === SymbolKind.Interface) {
-        const parent = symbol as any;
-        items.push(item);
-
-        if ("children" in parent && parent.children.length) {
-          for (const child of (symbol as any).children) {
-            items.push(this._getItem(child, 1));
-          }
-        }
-      } else {
-        items.push(item);
-      }
-    }
-
-    this._quickPick.items = items;
-
-    let lastItem: QuickItem;
-
-    this._quickPick.onDidChangeActive((items) => {
-      let item = items[0];
-      lastItem = item;
-      let symbol = item.symbol;
-
-      if (window.activeTextEditor) {
-        window.activeTextEditor.revealRange(symbol.location.range);
-
-        const startOffset = window.activeTextEditor.document.offsetAt(symbol.location.range.start);
-        const tokenOffset = window.activeTextEditor.document
-          .getText(symbol.location.range)
-          .indexOf(symbol.name) + startOffset;
-
-        const tokenPosition = window.activeTextEditor.document.positionAt(tokenOffset);
-        const tokenRange = window.activeTextEditor.document.getWordRangeAtPosition(tokenPosition);
-
-        const end = new Position(
-          tokenPosition.line,
-          tokenPosition.character + 10
-        );
-        const outRange = new Range(
-          tokenPosition,
-          end
-        );
-        window.activeTextEditor.setDecorations(selectionStyle, [tokenRange!]);
-      }
-    });
-    this._quickPick.onDidAccept(() => {
-      const item = lastItem;
-      const symbol = item.symbol;
-
-      if (window.activeTextEditor) {
-        window.activeTextEditor.revealRange(symbol.location.range);
-
-        const startOffset = window.activeTextEditor.document.offsetAt(symbol.location.range.start);
-        const tokenOffset = window.activeTextEditor.document
-          .getText(symbol.location.range)
-          .indexOf(symbol.name) + startOffset;
-
-        const tokenPosition = window.activeTextEditor.document.positionAt(tokenOffset);
-        const tokenRange = window.activeTextEditor.document.getWordRangeAtPosition(tokenPosition);
-
-        const end = new Position(
-          tokenPosition.line,
-          tokenPosition.character + 10
-        );
-        const outRange = new Range(
-          tokenPosition,
-          end
-        );
-
-        window.activeTextEditor.selection = new Selection(symbol.location.range.start, symbol.location.range.start);
-        window.activeTextEditor.setDecorations(selectionStyle, []);
-      }
-
-      this._quickPick.dispose();
-    });
-
+    this._quickPick.onDidChangeActive((items) => this._onDidChangeActive(items as any));
+    this._quickPick.onDidAccept(() => this._onDidAccept());
     this._quickPick.onDidChangeSelection((value) => {
       console.log("select", value);
     });
-
     this._quickPick.onDidChangeValue((value) => {
-      console.log(value);
-
+      console.log("change value", value);
     });
 
     this._quickPick.show();
@@ -153,138 +102,45 @@ export class QuickOutline {
     this._quickPick.dispose();
   }
 
-  private _iconForKind(kind: SymbolKind) {
-    switch (kind) {
-      case SymbolKind.Array: return `$(symbol-array)`;
-      case SymbolKind.Boolean: return `$(symbol-boolean)`;
-      case SymbolKind.Constant: return `$(symbol-constant)`;
-      case SymbolKind.Class: return `$(symbol-class)`;
-      case SymbolKind.Constructor: return `$(symbol-constructor)`;
-      case SymbolKind.Enum: return `$(symbol-enum)`;
-      case SymbolKind.EnumMember: return `$(symbol-enum-member)`;
-      case SymbolKind.Event: return `$(symbol-event)`;
-      case SymbolKind.Field: return `$(symbol-field)`;
-      case SymbolKind.File: return `$(symbol-file)`;
-      case SymbolKind.Function: return `$(symbol-function)`;
-      case SymbolKind.Interface: return `$(symbol-interface)`;
-      case SymbolKind.Key: return `$(symbol-key)`;
-      case SymbolKind.Module: return `$(symbol-module)`;
-      case SymbolKind.Method: return `$(symbol-method)`;
-      case SymbolKind.Namespace: return `$(symbol-namespace)`;
-      case SymbolKind.Null: return `$(symbol-null)`;
-      case SymbolKind.Number: return `$(symbol-number)`;
-      case SymbolKind.Object: return `$(symbol-object)`;
-      case SymbolKind.Operator: return `$(symbol-operator)`;
-      case SymbolKind.Package: return `$(symbol-package)`;
-      case SymbolKind.Property: return `$(symbol-property)`;
-      case SymbolKind.String: return `$(symbol-string)`;
-      case SymbolKind.Struct: return `$(symbol-struct)`;
-      case SymbolKind.TypeParameter: return `$(symbol-type-parameter)`;
-      case SymbolKind.Variable: return `$(symbol-variable)`;
-    }
+  private _quickPick = window.createQuickPick<QuickOutlineItem>();
+  private _lastItem: QuickOutlineItem | null = null;
+  private _editor = window.activeTextEditor!;
+
+  private _onDidChangeActive(items: QuickOutlineItem[]) {
+    let item = items[0];
+    this._lastItem = item;
+
+    const nameRange = item.getNameRange(this._editor.document);
+
+    this._editor.revealRange(item.location.range);
+    this._editor.setDecorations(selectionStyle, [nameRange]);
   }
 
-  private _kindToText(symbol: SymbolInformation): string {
-    switch (symbol.kind) {
-      case SymbolKind.Array: return `array`;
-      case SymbolKind.Boolean: return `boolean`;
-      case SymbolKind.Struct: return `struct`;
-      case SymbolKind.Interface: return `interface`;
-      case SymbolKind.Class: return `class`;
-      case SymbolKind.Constructor: return `constructor`;
-      case SymbolKind.Enum: return `enum`;
-      case SymbolKind.Event: return `event`;
-      case SymbolKind.Field: return `field`;
-      case SymbolKind.File: return `file`;
-      case SymbolKind.Method: return "method";
-      case SymbolKind.Function: return "function";
-      case SymbolKind.Constant: return "constant";
-      case SymbolKind.Variable: return "variable";
-      case SymbolKind.EnumMember: return "enum member";
-      case SymbolKind.Property: return "property";
-      case SymbolKind.TypeParameter: return "type parameter";
-      case SymbolKind.Key: return `key`;
-      case SymbolKind.Module: return `module`;
-      case SymbolKind.Namespace: return `namespace`;
-      case SymbolKind.Null: return `null`;
-      case SymbolKind.Number: return `number`;
-      case SymbolKind.Object: return `object`;
-      case SymbolKind.Operator: return `operator`;
-      case SymbolKind.Package: return `package`;
-      case SymbolKind.String: return `string`;
+  private _onDidAccept(): void {
+    const item = this._lastItem;
+
+    if (item == null) {
+      return;
     }
+
+    const nameRange = item.getNameRange(this._editor.document);
+
+    this._editor.selection = new Selection(item.location.range.start, item.location.range.start);
+    this._editor.revealRange(item.location.range);
+    this._editor.setDecorations(selectionStyle, []);
+
+    this.destroy();
   }
 
-  private _textForSymbol(symbol: SymbolInformation): string {
-    const kind = symbol.kind;
+  private _extractExpandedItems(items: QuickOutlineItem[], out: QuickOutlineItem[] = []): QuickOutlineItem[] {
+    for (const item of items) {
+      out.push(item);
 
-    if (window.activeTextEditor == undefined) {
-      return "";
+      if (item.expanded) {
+        this._extractExpandedItems(item.children, out);
+      }
     }
 
-    switch (kind) {
-      case SymbolKind.Array: return `array`;
-      case SymbolKind.Boolean: return `boolean`;
-      case SymbolKind.Struct: return `struct`;
-      case SymbolKind.Interface: return `interface`;
-      case SymbolKind.Class: return `class`;
-      case SymbolKind.Constructor: return `constructor`;
-      case SymbolKind.Enum: return `enum`;
-      case SymbolKind.Event: return `event`;
-      case SymbolKind.File: return `file`;
-      case SymbolKind.Method: {
-        const start = symbol.location.range.start;
-        const line = window.activeTextEditor.document.lineAt(start.line);
-        const lineText = window.activeTextEditor.document.getText(line.range)
-          .replace(/(.*)(\{)/s, '$1');
-
-        return lineText.trimStart();
-      }
-      case SymbolKind.Function: {
-        const start = symbol.location.range.start;
-        const line = window.activeTextEditor.document.lineAt(start.line);
-        const lineText = window.activeTextEditor.document.getText(line.range)
-          .replace(/(.*)(\{)/s, '$1');
-
-        return lineText.trimStart();
-      }
-      case SymbolKind.Field:
-      case SymbolKind.Constant:
-      case SymbolKind.Variable:
-      case SymbolKind.EnumMember:
-      case SymbolKind.Property:
-      case SymbolKind.TypeParameter: {
-        const startOffset = window.activeTextEditor.document.offsetAt(symbol.location.range.start);
-        const tokenOffset = window.activeTextEditor.document
-          .getText(symbol.location.range)
-          .indexOf(symbol.name) + startOffset;
-
-        const tokenPosition = window.activeTextEditor.document.positionAt(tokenOffset);
-        const tokenRange = window.activeTextEditor.document.getWordRangeAtPosition(tokenPosition);
-        const tokenEnd = tokenRange!.end;
-
-        const start = symbol.location.range.start;
-        const line = window.activeTextEditor.document.lineAt(start.line);
-        const range = new Range(tokenEnd, line.range.end);
-        const lineText = window.activeTextEditor.document.getText(range)
-          .replace(/^[^a-zA-Z0-9]+/, '')
-          .replace(/(.*)(,)/s, '$1')
-          .replace(/(.*)(;)/s, '$1');
-
-        return lineText;
-      }
-
-      case SymbolKind.Key: return `key`;
-      case SymbolKind.Module: return `module`;
-      case SymbolKind.Namespace: return `namespace`;
-      case SymbolKind.Null: return `null`;
-      case SymbolKind.Number: return `number`;
-      case SymbolKind.Object: return ``;
-      case SymbolKind.Operator: return `operator`;
-      case SymbolKind.Package: return `package`;
-      case SymbolKind.String: return `string`;
-    }
+    return out;
   }
-
-
 }
